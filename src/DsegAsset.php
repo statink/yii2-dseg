b75347f4ea9d33fbc41ad6c143abc4758405ee82
AIZAWA Hina <hina@bouhime.com>
2018-11-19 03:39:06 +0900

    init
    Signed-off-by: AIZAWA Hina <hina@bouhime.com>

---
+++ src/DsegAsset.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015-2018 AIZAWA Hina
+ * @license https://github.com/fetus-hina/stat.ink/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+declare(strict_types=1);
+
+namespace statink\yii2\dseg;
+
+class DsegAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@npm/dseg',
+    public $css = [
+        'css/dseg.css',
+    ];
+    public $publishOptions = [
+        'only' => [
+            'fonts/*',
+            'css/*',
+        ],
+    ];
+}
